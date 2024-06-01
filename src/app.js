44e95b3c7ce753a879991549addf1b0412ec59c9
satinderkumar21 <satinder21ku@gmail.com>
2024-06-01 10:25:07 +0530

    add register control and bug fixes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
+    methods: ['GET','POST','DELETE','PUT'],
     credentials: true
 }))
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true, limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser)
+// app.use(express.json({limit:"16kb"}))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+// app.use(express.static("public"))
+app.use(cookieParser())
+
+// console.log("first--2")
+// app.get("/", (req,res)=>{
+//     console.log(" in / route")
+//     res.status(200).send("Hello from Server");
+// })
 
 //routes import
 import userRouter from './routes/user.routes.js'
@@ -21,6 +29,6 @@ import userRouter from './routes/user.routes.js'
 // routes declartion
 app.use("/api/v1/users", userRouter)
 
-//http://localhost:800/api/v1/users/register
+//http://localhost:80001/api/v1/users/register
 
-export { app }
+export default app;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,23 @@
-import { Router } from "express"; 
+import express from "express";
+import { upload } from "../middlewares/multer.middleware.js";
 
 // import { registerUser } from "../controllers/user.controller.js";
 import { registerUser } from "../controllers/user.controller.js";
-const router = Router();
+import { ApiError } from "../utils/ApiError.js";
+const router = express.Router();
 
-router.route("/register").post(registerUser);
+
+router.route("/register").post(
+    upload.fields([
+        { name : "avatar",
+          maxCount: 1
+        },
+        {
+            name : "coverImage",
+            maxCount:1
+        }
+    ]),
+    
+    registerUser);
 
 export default router
